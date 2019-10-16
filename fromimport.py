b01e9b5aa2107419cabc1a06b4ca5efc954735ed
andidhetryansah <49136561+andidhetryansah@users.noreply.github.com>
2019-10-16 21:31:07 +0800

    Create fromimport.py

---
+++ fromimport.py
@@ -0,0 +1,17 @@
+from geometri2D import luasPersegiPanjang
+
+def main():
+
+    p = 10
+    l = 8
+
+    luas = luasPersegiPanjang(p, l)
+
+    print("Persegi Panjang")
+    print("Panjang\t\t: ", p)
+    print("Lebar\t\t: ",l)
+    print("Luas\t\t: ", luas)
+
+
+if __name__ == "__main__":
+    main()
