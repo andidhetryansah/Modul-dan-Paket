2ff731a030143f1f777ab70b3d0187c824def403
andidhetryansah <49136561+andidhetryansah@users.noreply.github.com>
2019-10-16 22:30:37 +0800

    Create tugas.py

---
+++ tugas.py
@@ -0,0 +1,15 @@
+import fungsi
+
+def main():
+     s = 16
+     t = 8
+     
+     w = fungsi.gerak(s, t)
+
+     print ("KECEPATAN")
+     print ("Jarak\t: ", s)
+     print ("Waktu Tempu\t: ", t)
+     print ("Kecepatan\t =", w)
+
+if __name__ == "__main__":
+    main()
