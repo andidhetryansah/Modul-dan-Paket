7db65697a1e33c58c2451d1df04c8e681d4e2997
andidhetryansah <49136561+andidhetryansah@users.noreply.github.com>
2019-10-16 21:57:30 +0800

    Create paket1.py

---
+++ paket1.py
@@ -0,0 +1,14 @@
+from rian.geometri2D import luasBujurSangkar
+
+def main():
+
+    sisi = 5
+
+    luas = luasBujurSangkar(sisi)
+
+    print("BUJURSANGKAR")
+    print("Panjang sisi \t : ", sisi)
+    print("Luas\t\t = ", luas)
+
+if __name__ == "__main__":
+    main()
