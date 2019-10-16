8a4cac79fffe0ca9e3289f5244fd21f8de32e039
andidhetryansah <49136561+andidhetryansah@users.noreply.github.com>
2019-10-16 20:59:33 +0800

    Create geometri2D.py

---
+++ geometri2D.py
@@ -0,0 +1,24 @@
+import math
+def luasPersegiPanjang(p, l):
+   return p * l
+
+def KelilingPersegiPanjang(p, l):
+   return 2 * (p+l)
+
+def luasBujursangkar(s):
+   return s * s
+
+def KelilingBujursangkar(s):
+   return 4 * s
+
+def luasLingkaran(r):
+   return math.pi * r * r
+
+def kelilingLingkaran(r):
+   return 2 * math.pi * r
+
+def luassegitiga (a, t):
+   return (a * t) / 2
+   
+def kelilingsegitiga(a, b, c):
+   return a + b + c
